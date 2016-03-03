b1088f5cc9340555828358733ae946edfac7b197
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-03-03 04:40:16 -0500

    runMultiple update

--- runMultiple.sh
+++ runMultiple.sh
@@ -7,12 +7,11 @@ HIDDEN_MAX="$4"
 
 while [ $LAMBDA_MIN -lt $LAMBDA_MAX ]; do
 	while [ $HIDDEN_MIN -lt $HIDDEN_MAX ]; do
-        	
-             	let HIDDEN_MIN=$HIDDEN_MIN+5
 		echo "Running nnScript for ${HIDDEN_MIN} Hidden nodes and ${LAMBDA_MIN} Lambda";
 		FileName="outPut_${HIDDEN_MIN}_${LAMBDA_MIN}.txt";
 		echo "Output File : ${FileName}";
 		python args.py $LAMBDA_MIN $HIDDEN_MIN > $FileName
+           	let HIDDEN_MIN=$HIDDEN_MIN+5
         done
 	echo The LambdaMin is $LAMBDA_MIN
 	let LAMBDA_MIN=$LAMBDA_MIN+2
